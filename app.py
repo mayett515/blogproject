1d1d6c335a23b5e885b624a793f5f50e3570f48c
mayett515 <matthias.vmach@gmail.com>
2024-11-11 03:54:59 +0100

    add function added

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import json
 import os
 
@@ -21,6 +21,35 @@ def index():
     loaded_json_blog_posts = load_blog_posts()
     blog_posts = loaded_json_blog_posts['posts']
     return render_template('index.html', posts=blog_posts)
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add():
+    if request.method == 'GET':
+        return render_template('add.html')
+
+    elif request.method == 'POST':
+        new_post = {
+            'author': request.form.get('author', ''),
+            'title': request.form.get('title', ''),
+            'content': request.form.get('content', '')
+        }
+
+        blog_data = load_blog_posts()
+        blog_posts = blog_data.get('posts', [])
+
+        if blog_posts:
+            new_post['id'] = max(post['id'] for post in blog_posts) + 1
+        else:
+            new_post['id'] = 1
+
+        blog_posts.append(new_post)
+
+        with open(os.path.join('data', 'blog_post.json'), 'w') as f:
+            json.dump({'posts': blog_posts}, f, indent=4)
+
+        return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
